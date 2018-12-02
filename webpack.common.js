46d776f1af6f611f3ccbd0b0fd551ea6c0a965e5
Cuong <ngoccuonggl249@gmail.com>
2018-12-02 21:43:46 +0700

    js loader

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,14 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
         test: /\.(png|jp(e*)g|svg)$/,
         use: [
           {
